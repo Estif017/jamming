037bb6027df498e24bf9b86dade1e25d99939b84
unknown <estifoteklearegay@gmail.com>
2021-05-03 11:06:01 -0700

    created app component

---
+++ src/Components/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>
+					Ja<span className='highlight'>mmm</span>ing
+				</h1>
+				<div className='App'>
+					{/* Add SearchBar Component */}
+					<div className='App-playlist'>
+						{/* Add a Search component */}
+						{/* Add a Playlist component */}
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
